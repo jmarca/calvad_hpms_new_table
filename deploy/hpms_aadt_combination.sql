e4bdaffb96cb674f381f8a8132555399d027637f
James E. Marca <james@activimetrics.com>
2016-12-17 08:48:59 -0800

    okay, do the pk fix properly this time!
    duh.  needed to modify the table, not just create.

--- deploy/hpms_aadt_combination.sql
+++ deploy/hpms_aadt_combination.sql
@@ -3,16 +3,8 @@
 
 BEGIN;
 SET search_path TO hpms,public;
+alter table hpms.hpms_aadt_combination drop constraint "hpms_aadt_combination_pkey";
 
-CREATE TABLE hpms_aadt_combination (
-      segment_id integer references hpms_segments (sid),
-      year_record smallint,
-      aadt double precision,
-      comments character varying(100),
-      last_modified_on date,
-      last_modified_by character varying(100),
-      data_source smallint,
-      primary key (segment_id,year_record)
-      );
+alter table hpms.hpms_aadt_combination add primary key (segment_id,year_record);
 
 COMMIT;
--- deploy/hpms_aadt_single_unit.sql
+++ deploy/hpms_aadt_single_unit.sql
@@ -5,16 +5,9 @@ BEGIN;
 
 SET search_path TO hpms,public;
 
-CREATE TABLE hpms_aadt_single_unit (
-      segment_id integer references hpms_segments (sid),
-      year_record smallint,
-      aadt double precision,
-      comments character varying(100),
-      last_modified_on date,
-      last_modified_by character varying(100),
-      data_source smallint,
-      primary key (segment_id,year_record)
-      );
+alter table hpms.hpms_aadt_single_unit drop constraint "hpms_aadt_single_unit_pkey";
+
+alter table hpms.hpms_aadt_single_unit add primary key (segment_id,year_record);
 
 
 COMMIT;
